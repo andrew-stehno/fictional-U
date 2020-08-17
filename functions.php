dacef72a8090d1ae9c75b6e2d3a1a195bbb7ee16
andrew stehno <andrew_stehno@yahoo.com>
2020-08-17 12:35:56 -0600

    initial commit

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php 
+
+    function university_files() {
+        wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i" rel="stylesheet');
+        wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+
+        if (strstr($_SERVER['SERVER_NAME'], 'fictional-university.local')) {
+            wp_enqueue_script('main-univeristy-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+        } else {
+            wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), NULL, '1.0', true);
+            wp_enqueue_script('main-univeristy-js', get_theme_file_uri('/bundled-assets/scripts.291f4fbd3120f33dcc5a.js'), NULL, '1.0', true);
+            wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.291f4fbd3120f33dcc5a.css'));
+        }
+
+
+    }
+
+    add_action('wp_enqueue_scripts', 'university_files');
+
+    function university_features() {
+        add_theme_support('title-tag');
+    }
+    
+    add_action('after_setup_theme', 'university_features');
+?>
